f371b3d7b7165bfaab0e4c29c1dd0a1f6831646f
Jason Summers <jason.summers467@gmail.com>
2019-10-11 21:05:18 -0500

    imported assemblygit status

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace spindll
     {
         static void Main(string[] args)
         {
-			ClassInspector.LoadDll(@"/home/myr/_repo/f5saver/f5saver.api/F5Saver.Common/bin/Debug/netstandard2.0/F5Saver.Common.dll");
-            // ClassInspector.LoadDll(@"/home/myr/_repo/spindll/bin/Debug/netcoreapp3.0/spindll.dll");
+			ClassInspector.LoadDll(@"D:\_repo\f5saver\f5saver.api\F5Saver.Common\bin\Debug\netstandard2.0\F5Saver.Common.dll");
+			// ClassInspector.LoadDll(@"/home/myr/_repo/f5saver/f5saver.api/F5Saver.Common/bin/Debug/netstandard2.0/F5Saver.Common.dll");
         }
     }
 }
--- spindll/ClassInspector.cs
+++ spindll/ClassInspector.cs
@@ -9,6 +9,23 @@ namespace spindll.logic
         {
             var DLL = Assembly.LoadFile(filePath);
 
+			// var t = DLL.GetType("Question");
+			// var activator = Activator.CreateInstance(t, false, false);
+
+			var name = DLL.FullName;
+			var modules = DLL.Modules;
+			var common = DLL.GetModules();
+			var com = common.Length;
+
+			// var dllmod = DLL.DefinedTypes;
+
+			var commonModule = modules.First();
+			// var types = commonModule.Assembly.DefinedTypes;
+
+			var _type = DLL.GetType("F5Saver.Common.Models.Question");
+			var _type2 = DLL.GetType("F5Saver.Common.Models.User");
+
+			// var _types = DLL.GetTypes();
 			var obj = DLL.GetExportedTypes();
 
             foreach(Type type in DLL.GetExportedTypes())
