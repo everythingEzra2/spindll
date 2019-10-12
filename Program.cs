1f47b6e4ca9d9e8df6335aecf661ca6b9652db21
jason summers <jason.summers467@gmail.com>
2019-10-12 14:54:34 -0500

    breakthrough

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using spindll.logic;
 
 namespace spindll
@@ -7,8 +9,43 @@ namespace spindll
     {
         static void Main(string[] args)
         {
-			ClassInspector.LoadDll(@"D:\_repo\f5saver\f5saver.api\F5Saver.Common\bin\Debug\netstandard2.0\F5Saver.Common.dll");
-			// ClassInspector.LoadDll(@"/home/myr/_repo/f5saver/f5saver.api/F5Saver.Common/bin/Debug/netstandard2.0/F5Saver.Common.dll");
+			// ClassInspector.LoadDll(@"D:\_repo\f5saver\f5saver.api\F5Saver.Common\bin\Debug\netstandard2.0\F5Saver.Common.dll");
+			var types = ClassInspector.LoadDll(@"/home/myr/_repo/f5saver/f5saver.api/F5Saver.Common/bin/Debug/netstandard2.0/F5Saver.Common.dll").ToList()S;
+
+			var models = extractModels(types);
         }
+
+		static List<string> extractModels(List<Type> types) 
+		{
+
+			var typeList = types.ToList();
+			typeList.ForEach(t => {
+				var models 
+			});
+		}
     }
+}
+
+namespace spindll.Models {
+	class EnumInfo {
+		public string EnumName;
+		public Dictionary<int, string> Values;
+	}
+
+	class ModelInfo {
+		public ModelInfo() {}
+		public ModelInfo(Type type) {
+			ModelName = type.FullName;
+
+			var properties = type.GetProperties();
+			
+		}
+		public string ModelName;
+		public List<PropertyInfo> Properties;
+	}
+
+	class PropertyInfo {
+		public string PropertyName;
+		public string DataType;
+	}
 }
--- spindll/ClassInspector.cs
+++ spindll/ClassInspector.cs
@@ -1,40 +1,43 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace spindll.logic 
 {
     public static class ClassInspector
     {
-        public static void LoadDll(string filePath)
+        public static Type[] LoadDll(string filePath)
         {
             var DLL = Assembly.LoadFile(filePath);
 
 			// var t = DLL.GetType("Question");
 			// var activator = Activator.CreateInstance(t, false, false);
 
-			var name = DLL.FullName;
-			var modules = DLL.Modules;
-			var common = DLL.GetModules();
-			var com = common.Length;
+			// var name = DLL.FullName;
+			// var modules = DLL.Modules;
+			// var common = DLL.GetModules();
+			// var com = common.Length;
 
 			// var dllmod = DLL.DefinedTypes;
 
-			var commonModule = modules.First();
+			// var commonModule = modules.First();
 			// var types = commonModule.Assembly.DefinedTypes;
 
 			var _type = DLL.GetType("F5Saver.Common.Models.Question");
 			var _type2 = DLL.GetType("F5Saver.Common.Models.User");
 
-			// var _types = DLL.GetTypes();
+			var _types = DLL.GetTypes();
 			var obj = DLL.GetExportedTypes();
 
-            foreach(Type type in DLL.GetExportedTypes())
-            {
-                dynamic c = Activator.CreateInstance(type);
-                c.Output(@"Hello");
-            }
+			return _types;
 
-            Console.ReadLine();
+            // foreach(Type type in DLL.GetExportedTypes())
+            // {
+
+            // }
+
+            // Console.ReadLine();
         }
     }
 }
