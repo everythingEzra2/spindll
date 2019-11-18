9d499bc99bee6c3546944bdd788ad9c1f37fc1d6
Jason Summers <jason.summers467@gmail.com>
2019-11-17 22:32:54 -0600

    added checklist

--- spindll/Models/TestModel.cs
+++ spindll/Models/TestModel.cs
@@ -8,10 +8,10 @@ namespace spindll.Models
 
 	class Dog
 	{
-		public bool fuzzy { get; set; }
-		public bool borks { get; set; }
-		public string name { get; set; }
-		public DateTime BirthDate { get; set;}
+		public bool Fuzzy { get; set; }
+		public bool Borks { get; set; }
+		public string Name { get; set; }
+		public DateTime BirthDate { get; set; }
 	}
 
 }
